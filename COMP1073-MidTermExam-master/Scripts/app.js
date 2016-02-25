ae4b981a44ab28c2d512a6dd1704a22aaac6c6ab
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-02-25 09:34:07 -0400

    Alonissos injected into HtMl using JavaScript

--- COMP1073-MidTermExam-master/Scripts/app.js
+++ COMP1073-MidTermExam-master/Scripts/app.js
@@ -10,6 +10,10 @@ document.getElementById("IntroParagraph").innerHTML = "Now is the time to travel
 
 var Folegandros;
 Folegandros = document.getElementById("Folegandros");
-document.getElementById("Folegandros").innerHTML = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets."
+document.getElementById("Folegandros").innerHTML = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets.";
+
+var Alonissos;
+Alonissos = document.getElementById("Alonissos");
+document.getElementById("Alonissos").innerHTML = "Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy.";
 })();
 
