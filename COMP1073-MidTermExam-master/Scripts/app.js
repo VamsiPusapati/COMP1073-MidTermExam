b49b412595f5f77bad43100534ea0a4a4c7d04c3
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-02-25 10:03:59 -0400

    Gavdos injected into HTML using JavaScript

--- COMP1073-MidTermExam-master/Scripts/app.js
+++ COMP1073-MidTermExam-master/Scripts/app.js
@@ -43,5 +43,7 @@ document.getElementById("Ithaca").innerHTML = "Most famous for being the home of
 var Gavdos;
 Gavdos = document.getElementById("Gavdos");
 document.getElementById("Gavdos").innerHTML = "Near the more well-known Crete, Gavdos is the most southern island in Greece — and the most southern spot in Europe discounting the Canaries. Only accessible by ferry, the remote island has only about 50 permanent residents, and can feel like your private playground. Local legend has it that the island was the home of goddess Calypso, who kept Odysseus prisoner here. Today, a favorite tourist activity is to visit the spot believed to be her cave. Be warned, you won't find any luxury hotels here. While the romantically under-developed, super laid back island has a number of rooms for rent, those are limited, as the real draw here for many is the free, seaside camping.";
+
+
 })();
 
